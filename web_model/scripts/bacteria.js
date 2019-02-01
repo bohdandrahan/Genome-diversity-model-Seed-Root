2d14aad35aa754f84db0ba4210744112871b1306
bohdandrahan <bohdan.drahan@gmail.com>
2019-01-31 23:43:26 -0500

    add gitignore

--- web_model/scripts/bacteria.js
+++ web_model/scripts/bacteria.js
@@ -1,34 +1,33 @@
-class Bacteria extends Animal{
+class Bacteria extends Animal {
 
-  setMaxSpeed(maxspeed = random(10)){
-    this.maxspeed = maxspeed 
-  }
-  setNutritionValues(values = []){
-    this.nutritionValues = values 
-  }
-  setDnaLen(dnaLen = 0){
-    this.dnaLen = dnaLen;
-  }
-  setMaxForce(maxforce = 0.5) {
-    this.maxforce = maxforce
-  }
+	setMaxSpeed(maxspeed = random(10)) {
+		this.maxspeed = maxspeed
+	}
+	setNutritionValues(values = []) {
+		this.nutritionValues = values
+	}
+	setDnaLen(dnaLen = 0) {
+		this.dnaLen = dnaLen;
+	}
+	setMaxForce(maxforce = 0.5) {
+		this.maxforce = maxforce
+	}
 
-  setHealthDrop(hD = 0){
-    this.healthDrop = hD
-  }
-  behavior(groupsToEat, groupsToAvoid){
-  }
+	setHealthDrop(hD = 0) {
+		this.healthDrop = hD
+	}
+	behavior(groupsToEat, groupsToAvoid) {}
 
-  display() {
-    // Draw a worm
+	display() {
+		// Draw a worm
 
-    var theta = this.velocity.heading() + PI / 2;
-    push();
-    translate(this.position.x, this.position.y);
-    colorMode(HSB);
-    fill(this.clr, 95, 95);
-    noStroke();
-    ellipse(0,0, 20);
-    pop();
-  }
+		var theta = this.velocity.heading() + PI / 2;
+		push();
+		translate(this.position.x, this.position.y);
+		colorMode(HSB);
+		fill(this.clr, 95, 95);
+		noStroke();
+		ellipse(0, 0, 20);
+		pop();
+	}
 }
